6eb739d8da0d2e962bb5e50afbc7da8faa37bce8
AjayACST <ajay@acst.co.nz>
2021-08-16 10:46:37 +1200

    Added base code and typescript setup

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, {Express} from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import Router from './routes';
+import swaggerUi from "swagger-ui-express";
+import swaggetDocument from "./public/swagger.json";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+app.use(helmet());
+app.use(express.static("public"));
+
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggetDocument)
+);
+
+app.use(Router)
+
+app.listen(PORT, () => console.log(`Running on ${PORT}`))
